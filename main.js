c3cb7fe35a80112ac87fcc1fe52571210b001093
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-02-09 23:26:21 +0100

    final commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const root = new Vue (
+    {
+        el: '#root',
+        data: {
+            message: 'Qualcuno volò sul nido del cuculo',
+            boxClass: 'box',
+            img: 'https://www.gelestatic.it/thimg/ve1nnrMXWH2gjtgjO854nx_s4t0=/1280x0/smart/filters:format(webp)/https://www.repstatic.it/content/nazionale/img/2018/08/31/213348388-c82a6637-4909-4e17-8500-44d105bfce5a.jpg'
+        }
+    }
+);
